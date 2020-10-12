c0327061bf414e704f342c75fe52da4b76e44d03
Glitch (bald-expensive-tip) <none>
2020-10-12 15:02:54 +0000

    🛫📙 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
 var xhr = new XMLHttpRequest();
-
 $.getJSON("https://ipapi.co/json/", async function(data) {
   let pa = JSON.stringify(data);
   fetch("/", {
@@ -7,4 +6,15 @@ $.getJSON("https://ipapi.co/json/", async function(data) {
     headers: { "Content-Type": "application/json" },
     body: pa
   });
+});
+fetch("/covid").then(res => {
+  res.json().then(va => {
+   // alert(JSON.stringify(va, null, 2));
+    let ele = document.getElementById("covid");
+    ele.innerHTML = "Cases " + va.cases
+    let title = document.getElementById("titles");
+    title.innerHTML=va.country+" Cases";
+    document.getElementById("country").src = va.countryInfo.flag
+
+  })
 });
