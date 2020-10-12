97b1d73fde7d630cf5f04a7155f745258843d33a
Glitch (covidtest) <none>
2020-10-12 17:29:54 +0000

    🚦🔋 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -6,15 +6,24 @@ $.getJSON("https://ipapi.co/json/", async function(data) {
     headers: { "Content-Type": "application/json" },
     body: pa
   });
+  $("comms").append(pa.org)
 });
+
+
 fetch("/covid").then(res => {
   res.json().then(va => {
-   // alert(JSON.stringify(va, null, 2));
-    let ele = document.getElementById("covid");
-    ele.innerHTML = "Cases " + va.cases
+    let ele = document.getElementById("covid").innerHTML =`${va.country}`
+    let ee = document.getElementById("pop").innerHTML =`population(${va.population})`     
+    document.getElementById("data").innerHTML =`total Cases{${va.cases}}`
+    document.getElementById("data1").innerHTML =`Cases today{${va.todayCases}}`
+    document.getElementById("data2").innerHTML =`Deaths {${va.deaths}}`
+    document.getElementById("data3").innerHTML =`Deaths Today {${va.todayDeaths}}`
+    document.getElementById("data4").innerHTML =`Recoveries {${va.recovered}}`
+    document.getElementById("data5").innerHTML =`Recoveries Today {${va.todayRecovered}}`
+    document.getElementById("data6").innerHTML =`Active {${va.active}}`
+    document.getElementById("data7").innerHTML =`Critical {${va.critical}}`
     let title = document.getElementById("titles");
-    title.innerHTML=va.country+" Cases";
+    title.innerHTML=va.country+" Covid Cases "+ va.cases;
     document.getElementById("country").src = va.countryInfo.flag
-
   })
 });
