8f635344854c03012a8f1105af8747968172b174
Glitch (covidtest) <none>
2020-10-16 14:41:54 +0000

    🐡🌊 Updated with Glitch

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const fs = require("fs");
 const api = require("covidapi");
 const weather = require("weather-js");
-
+const NewsAPI = require("newsapi");
+var key = new NewsAPI("df48a88c18cf4a1c8e8178e43e3bd927");
 api.settings({
   baseUrl:
     "https://disease.sh" | "https://api.caw.sh" | "https://corona.lmao.ninja"
@@ -16,11 +17,19 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-app.post("/", async (req, res) => {
-  let js = req.body;
+async function ait(){
+await app.post("/", async (req, res) => {
+  let js = await req.body;
   let ip = js.ip;
-  let ct = js.country_name;
-
+  let ct = await js.country_name
+  console.log(js.country_name)
+  await app.get("/news", (req, res) => {
+    let jsp = ["covid", "covid news", "covid 19", "covid 19 news", "world health organization"]
+    let mat = jsp[Math.floor(Math.random() * jsp.length)]
+    key.v2.everything({ q: mat, language: "en" }).then(response => {
+       res.json(response)
+    });
+  });
   await app.get("/weather", (req, res) => {
     weather.find({ search: ct, degreeType: "C" }, function(err, result) {
       if (err) console.log(err);
@@ -34,6 +43,8 @@ app.post("/", async (req, res) => {
     });
   });
 });
+}
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
+  ait()
 });
